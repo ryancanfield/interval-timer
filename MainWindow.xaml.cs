a3c3c09d10f392e8be73f43f6b7401a85fb7a1c2
ryancanfield <ryan.a.canfield@gmail.com>
2023-09-22 14:29:55 -0500

    add

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace IntervalTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetIntervalTime();
+            TickTimer.Interval = new TimeSpan(0, 0, 1);
+            TickTimer.Tick += new EventHandler(TickTimer_Tick);
+            TickTimer.Start();
+        }
+
+        private TimeSpan IntervalTime { get; set; }
+        private DateTime EndTime { get; set; }
+        DispatcherTimer TickTimer = new DispatcherTimer();
+        System.Media.SoundPlayer Player = new System.Media.SoundPlayer(@"C:\Windows\Media\Alarm10.wav");
+        private bool Playing = false;
+
+        private void obj_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if ((e.Key >= Key.D1 && e.Key <= Key.D9) ||
+                (e.Key >= Key.NumPad1 && e.Key <= Key.NumPad9))
+                {
+                    string s = e.Key.ToString();
+                    s = s.Substring(s.Length-1);
+                    IntervalTimeTextBox.Text = s;
+                    SetIntervalTime();
+                }
+                if (e.Key == Key.Space)
+                {
+                    ResetStartTime();
+                }
+            }
+            catch
+            {
+                ; // no action
+            }
+        }
+
+        private void TickTimer_Tick(object sender, EventArgs e)
+        {
+            SetElapsedTime();
+        }
+
+        private void SetElapsedTime()
+        {
+            TimeSpan timeRemaining = EndTime - DateTime.Now;
+            if (timeRemaining < TimeSpan.Zero)
+            {
+                TimeRemainingTextBox.Text = String.Format("-{0:m\\:ss}", timeRemaining);
+                if (!Playing)
+                {
+                    Playing = true;
+                    Player.PlayLooping();
+                }
+
+            }
+            else
+            {
+                TimeRemainingTextBox.Text = String.Format("{0:m\\:ss}", timeRemaining);
+                Player.Stop();
+                Playing = false;
+            }
+        }
+
+        private void SetIntervalTime()
+        {
+            IntervalTime = new TimeSpan(0, int.Parse(IntervalTimeTextBox.Text), 0);
+            ResetStartTime();
+            SetElapsedTime();
+        }
+
+        private void ResetStartTime()
+        {
+            EndTime = DateTime.Now + IntervalTime;
+            SetElapsedTime();
+        }
+    }
+}
